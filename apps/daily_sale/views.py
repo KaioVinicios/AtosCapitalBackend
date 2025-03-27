c5ff4518318a19a5d89a617d39b95b103ec52006
Kaio Vinícios <kvsgpro@outlook.com>
2025-03-27 16:29:33 -0300

    feat: remodel view to new main model for daily_sale

--- apps/daily_sale/views.py
+++ apps/daily_sale/views.py
@@ -1,39 +1,42 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
-from rest_framework.decorators import (
-    api_view,
-    authentication_classes,
-    permission_classes,
-)
 from django.http import JsonResponse
-from apps.enterprise.models import Enterprise
-from apps.branche.models import Branche
-from apps.daily_sale.models import DailySale
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
+from apps.core.models import Tbvendasdashboard
+from django.db.models import Q
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 @authentication_classes([JWTAuthentication])
-def show_daily_sales_by_date(request, enterprise_url):
+def show_daily_sales_by_date(request):
+    
+    # Filtra vendas onde vlvenda não é nulo, usando o banco atos_db
+    sales = Tbvendasdashboard.objects.using('atos_db').filter(~Q(vlvenda__isnull=True))
 
-    enterprise = get_object_or_404(Enterprise, url_name=enterprise_url)
-    branches = enterprise.branche.all()
-    sales = DailySale.objects.filter(branche__in=branches)
+    # Obtém as filiais únicas baseadas em nmfilial, usando o banco atos_db
+    unique_branches = (
+        Tbvendasdashboard.objects.using('atos_db')
+        .filter(~Q(vlvenda__isnull=True))
+        .values('nmfilial', 'nrcnpj')
+        .distinct()
+    )
 
     response_data = {
-        "enterprise": enterprise.name,
         "branches": [
-            {"id": branch.id, "name": branch.name, "cnpj": branch.cnpj}
-            for branch in branches
+            {
+                "name": branch['nmfilial'],  # Nome da filial
+                "cnpj": branch['nrcnpj'],   # CNPJ da filial
+            }
+            for branch in unique_branches
         ],
         "sales": [
             {
-                "id": sale.id,
-                "date": sale.date,
-                "value": sale.value,
-                "goal": sale.goal,
-                "branche_id": sale.branche.id,
-                "branche_name": sale.branche.name,
+                "id": sale.idvendas,        # ID da venda
+                "cnpj": sale.nrcnpj,        # CNPJ
+                "branche_name": sale.nmfilial,  # Nome da filial
+                "date": sale.dtvenda,       # Data da venda
+                "value": float(sale.vlvenda) if sale.vlvenda is not None else None,  # Valor da venda (convertido para float)
+                "goal": float(sale.txmeta) if sale.txmeta is not None else None,    # Meta (convertido para float)
             }
             for sale in sales
         ],
