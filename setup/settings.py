399d23be70086cfefbc6d97bbd6b83da6fa0cc60
Kaio Vinícios <kvsgpro@outlook.com>
2025-03-27 21:33:35 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "INSECURE")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://atoscapitalbackend.onrender.com']
+ALLOWED_HOSTS = ['atoscapitalbackend.onrender.com']
 
 
 # Application definition
