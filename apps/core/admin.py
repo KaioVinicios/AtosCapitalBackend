bc3af400b9ce2b6df3214b144ad4e77828d64ccd
Kaio Vinícios <kvsgpro@outlook.com>
2025-03-31 18:30:24 -0300

    feat: add dockerfile

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -8,22 +8,22 @@ admin.site.site_header = "Atos Capital"
 admin.site.site_title = "Atos Capital"
 admin.site.index_title = "Página de administração"
 
-@admin.register(Enterprise)
-class EnterpriseAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'cnpj')
-    search_fields = ('name', 'email', 'cnpj')
+# @admin.register(Enterprise)
+# class EnterpriseAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'email', 'cnpj')
+#     search_fields = ('name', 'email', 'cnpj')
 
-@admin.register(EmployeeUser)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'whatsapp_number', 'enterprise')
-    search_fields = ('name', 'whatsapp_number')
+# @admin.register(EmployeeUser)
+# class EmployeeAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'whatsapp_number', 'enterprise')
+#     search_fields = ('name', 'whatsapp_number')
 
-@admin.register(Branche)
-class BrancheAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cnpj', 'enterprise')
-    search_fields = ('name', 'cnpj')
+# @admin.register(Branche)
+# class BrancheAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'cnpj', 'enterprise')
+#     search_fields = ('name', 'cnpj')
 
-@admin.register(DailySale)
-class DailySaleAdmin(admin.ModelAdmin):
-    list_display = ('date', 'value', 'goal', 'branche')
-    list_filter = ('date', 'branche')
+# @admin.register(DailySale)
+# class DailySaleAdmin(admin.ModelAdmin):
+#     list_display = ('date', 'value', 'goal', 'branche')
+#     list_filter = ('date', 'branche')
