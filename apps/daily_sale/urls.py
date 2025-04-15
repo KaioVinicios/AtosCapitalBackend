2fcf19ac53369d1a9260447a30a0ec5bf679f838
Kaio Vinícios <kvsgpro@outlook.com>
2025-04-15 12:20:28 -0300

    feat: add new endpoint do require daily sale by date.

--- apps/daily_sale/urls.py
+++ apps/daily_sale/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.daily_sale.views import show_daily_sales_by_date
+from apps.daily_sale.views import require_all_daily_sales, require_daily_sales_by_date
 
 urlpatterns = [
     path(
         "require_daily_sales/",
-        show_daily_sales_by_date,
-        name="show_daily_sales_by_date",
-    )
+        require_all_daily_sales,
+        name="require_all_daily_sales"
+    ),
+    path(
+        "require_daily_sales_by_date/<str:date>/",
+        require_daily_sales_by_date, 
+        name="require_daily_sales")
 ]
--- apps/daily_sale/views.py
+++ apps/daily_sale/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 @authentication_classes([JWTAuthentication])
-def show_daily_sales_by_date(request):
+def require_all_daily_sales(request):
     
     # Filtra vendas onde vlvenda não é nulo, usando o banco atos_db
     sales = Tbvendasdashboard.objects.using('atos_db').filter(~Q(vlvenda__isnull=True))
@@ -17,6 +17,46 @@ def show_daily_sales_by_date(request):
     unique_branches = (
         Tbvendasdashboard.objects.using('atos_db')
         .filter(~Q(vlvenda__isnull=True))
+        .values('nmfilial', 'nrcnpj')
+        .distinct()
+    )
+
+    response_data = {
+        "branches": [
+            {
+                "name": branch['nmfilial'],  
+                "cnpj": branch['nrcnpj'],   
+            }
+            for branch in unique_branches
+        ],
+        "sales": [
+            {
+                "id": sale.idvendas,        
+                "cnpj": sale.nrcnpj,        
+                "branche_name": sale.nmfilial,
+                "date": sale.dtvenda,       
+                "value": float(sale.vlvenda) if sale.vlvenda is not None else None,  
+                "goal": float(sale.txmeta) if sale.txmeta is not None else None,    
+            }
+            for sale in sales
+        ],
+    }
+
+    return JsonResponse(response_data)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+@authentication_classes([JWTAuthentication])
+def require_daily_sales_by_date(request, date):
+
+    # date type string in format == year-month-day (YY-MM-DD)
+
+    sales = Tbvendasdashboard.objects.using('atos_db').filter(~Q(vlvenda__isnull=True), dtvenda = date)
+
+    unique_branches = (
+        Tbvendasdashboard.objects.using('atos_db')
+        .filter(~Q(vlvenda__isnull=True), dtvenda = date)
         .values('nmfilial', 'nrcnpj')
         .distinct()
     )
